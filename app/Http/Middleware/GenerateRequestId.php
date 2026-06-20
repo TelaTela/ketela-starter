d771d87d0c3496c8d33cdbb54846a5178c0e9432
TelaTela <40137857+TelaTela@users.noreply.github.com>
2026-06-20 20:21:31 +0700

    feat: generate request id

---
+++ app/Http/Middleware/GenerateRequestId.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenerateRequestId
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $requestId = (string) Str::uuid();
+
+        $request->attributes->set('request_id', $requestId);
+
+        return $next($request);
+    }
+}
