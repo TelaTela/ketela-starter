4d04b72d077a4b92ab6469dc7b224b9dce00b6f0
TelaTela <40137857+TelaTela@users.noreply.github.com>
2026-06-29 17:01:52 +0700

    lang: localize dashboard page

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,10 +1,13 @@
 import { Head, usePage } from '@inertiajs/react';
 import TimedGreeting from '@/components/timed-greeting';
 import { dashboard } from '@/routes';
+import { lang } from '@erag/lang-sync-inertia/react';
 
 export default function Dashboard() {
     const { auth } = usePage().props;
     const user = auth.user;
+
+    const { __ } = lang();
 
     return (
         <>
@@ -12,7 +15,7 @@ export default function Dashboard() {
             <div className="flex flex-row p-4">
                 <div className="flex flex-col">
                     <TimedGreeting />
-                    <h2 className="text-2xl font-medium">Hi, {user.name}</h2>
+                    <h2 className="text-2xl font-medium">{__('pages/dashboard.hi_user', { user: user.name })}</h2>
                 </div>
             </div>
         </>
