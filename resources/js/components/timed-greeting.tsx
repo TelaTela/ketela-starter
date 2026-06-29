3971b81f44e59100c030adb4fb469770e8f725df
TelaTela <40137857+TelaTela@users.noreply.github.com>
2026-06-29 17:07:11 +0700

    chore: remove unused code

--- resources/js/components/timed-greeting.tsx
+++ resources/js/components/timed-greeting.tsx
@@ -26,21 +26,7 @@ export default function TimedGreeting({
         }
     }
 
-    let greeting: string = '';
-
     const { __ } = lang();
-
-    switch (time) {
-        case 'morning':
-            greeting = 'Good Morning! 🌄';
-            break;
-        case 'noon':
-            greeting = 'Good Afternoon! 🌞';
-            break;
-        case 'evening':
-            greeting = 'Goon Evening! 🌛';
-            break;
-    }
 
     return <h1 className="text-3xl font-medium">{__('components/timed-greeting.' + time)}</h1>;
 }
