75ae717342974867ec0621d143e62f117a26ddcf
TelaTela <40137857+TelaTela@users.noreply.github.com>
2026-07-06 01:18:16 +0700

    fix: missing import

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Log;
 use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 /**
  * @property int $id
